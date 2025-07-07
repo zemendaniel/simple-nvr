10fca6a0fd9aec9c3517e1184bc61abeb399b50f
zemendaniel <zemendaniel@gmail.com>
2025-07-07 19:51:35 +0200

    working on sockets

--- audio/streamer.py
+++ audio/streamer.py
@@ -1,3 +1,5 @@
+import queue
+
 import sounddevice as sd
 import threading
 from extensions import sio
@@ -9,22 +11,24 @@ class AudioStreamer:
         self.sample_rate = sample_rate
         self.channels = channels
         self.chunk_size = chunk_size
-        self.thread = None
+        self.capture_thread = None
         self.room = 'audio_listeners'
         self.is_running = False
+        self.q = queue.Queue(maxsize=100)
 
     def start(self):
         if self.is_running:
             return
         self.is_running = True
-        self.thread = threading.Thread(target=self._stream_audio, daemon=True).start()
+        self.capture_thread = threading.Thread(target=self._capture_audio, daemon=True).start()
+        sio.start_background_task(self._stream_audio)
         print('[AudioStreamer] Audio thread started.')
 
     def stop(self):
         if not self.is_running:
             return
         self.is_running = False
-        self.thread.join(timeout=1)
+        self.capture_thread.join(timeout=1)
         print('[AudioStreamer] Audio thread stopped.')
 
     def add_client(self, sid):
@@ -36,13 +40,25 @@ class AudioStreamer:
         leave_room(self.room, sid=sid)
         # todo stop if empty
 
-    def _stream_audio(self):
+    def _capture_audio(self):
         try:
             with sd.InputStream(samplerate=self.sample_rate, channels=self.channels, dtype='int16') as stream:
                 while self.is_running:
                     data, _ = stream.read(self.chunk_size)
-                    sio.emit('audio_chunk', data.tobytes(), room=self.room)
-                    sio.sleep(0)
+                    try:
+                        self.q.put(data.tobytes())
+                    except queue.Full:
+                        pass
         except Exception as e:
             print(f'[AudioStreamer] Error: {e}')
 
+    def _stream_audio(self):
+        while self.is_running:
+            try:
+                data = self.q.get(timeout=0.1)
+                sio.emit('audio_chunk', data, to=self.room)
+            except queue.Empty:
+                continue  # check self.is_running again
+            except Exception as e:
+                print(f"[SocketIO Emit Error] {e}")
+            sio.sleep(0)
